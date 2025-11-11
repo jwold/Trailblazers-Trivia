11967e549b1f46f6761a7440c9b187f00a558eca
Nathan Isaac <jarvis.git@pm.me>
2025-11-11 08:07:33 -0800

    add test package

---
+++ TrailblazersTrivia/TrailblazersTriviaTests/TrailblazersTriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrailblazersTriviaTests.swift
+//  TrailblazersTriviaTests
+//
+//  Created by Tony Stark on 11/11/25.
+//
+
+import Testing
+
+struct TrailblazersTriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        #expect(true)
+    }
+
+}
